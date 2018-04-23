6a6f9db32aaf6235138ae62c944aabacc1fa3416
Jessica Giehl <sumdian@gmail.com>
2018-04-22 23:40:33 -0400

    Radio Buttons partially implemented

--- js/form.js
+++ js/form.js
@@ -3,6 +3,9 @@ const submit = document.querySelector('button');
 var formInputs = document.getElementsByTagName('input');
 var errorsPresent = false;
 
+//variables for storing form data
+var size = 'Small';
+var color = 'Red';
 var name = '';
 var email = '';
 var address = '';
@@ -10,11 +13,23 @@ var address2 = '';
 var city = '';
 var state = '';
 var zip = '';
-var countrySelector = document.getElementById('country');
-var country = countrySelector.options[countrySelector.selectedIndex].innerHTML;
-var shippingInfo = '';
+var country = '';
 
-var outputTag = document.getElementById('shippinginfo');
+//variables for DOM output
+var shippingInfo = '';
+var productInfo = '';
+
+//variables for storing document queries
+var smallRadio = document.getElementById('small');
+var mediumRadio = document.getElementById('medium');
+var largeRadio = document.getElementById('large');
+var redRadio = document.getElementById('red');
+var yellowRadio = document.getElementById('yellow');
+var greenRadio = document.getElementById('green');
+var blueRadio = document.getElementById('blue');
+var countrySelector = document.getElementById('country');
+var outputProduct = document.getElementById('productinfo');
+var outputShipping = document.getElementById('shippinginfo');
 
 // Create a validity class
 
@@ -57,15 +72,81 @@ class CheckValidity {
 
 }
 
-document.querySelector('#name').addEventListener("blur", function(){
-  name = document.querySelector('#name').value;
+smallRadio.addEventListener("click", function(){
+  if (smallRadio.checked){
+    size = small;
+  }
+  productInfo = `${size} ${color} Paint Can`;
+  outputProduct.innerHTML = productInfo;
+})
+
+mediumRadio.addEventListener("click", function(){
+  if (mediumRadio.checked){
+    size = medium;
+  }
+  productInfo = `${size} ${color} Paint Can`;
+  outputProduct.innerHTML = productInfo;
+})
+
+largeRadio.addEventListener("click", function(){
+  if (largeRadio.checked){
+    size = large;
+  }
+  var productInfo = `${size} ${color} Paint Can`;
+  outputProduct.innerHTML = productInfo;
+})
+
+redRadio.addEventListener("click", function(){
+  if (redRadio.checked){
+    color = red;
+  }
+  productInfo = `${size} ${color} Paint Can`;
+  outputProduct.innerHTML = productInfo;
+})
+yellowRadio.addEventListener("click", function(){
+  if (yellowRadio.checked){
+    color = yellow;
+  }
+  productInfo = `${size} ${color} Paint Can`;
+  outputProduct.innerHTML = productInfo;
+})
+greenRadio.addEventListener("click", function(){
+  if (greenRadio.checked){
+    color = green;
+  }
+  productInfo = `${size} ${color} Paint Can`;
+  outputProduct.innerHTML = productInfo;
+})
+blueRadio.addEventListener("click", function(){
+  if (blueRadio.checked){
+    color = blue;
+  }
+  productInfo = `${size} ${color} Paint Can`;
+  outputProduct.innerHTML = productInfo;
+})
+
+countrySelector.addEventListener("change", function(){
+  country = countrySelector.options[countrySelector.selectedIndex].innerHTML;
   var shippingInfo =
   `${name}
   ${email}
   ${address} ${address2}
   ${city} ${state} ${zip}
   ${country}`;
-  outputTag.innerHTML = shippingInfo;
+  outputShipping.innerHTML = shippingInfo;
+})
+
+document.querySelector('#name').addEventListener("blur", function(){
+  name = document.querySelector('#name').value;
+  //added incase they autofill form
+  country = countrySelector.options[countrySelector.selectedIndex].innerHTML;
+  var shippingInfo =
+  `${name}
+  ${email}
+  ${address} ${address2}
+  ${city} ${state} ${zip}
+  ${country}`;
+  outputShipping.innerHTML = shippingInfo;
 })
 document.querySelector('#email').addEventListener("blur", function(){
   email = document.getElementById('email').value;
@@ -75,7 +156,7 @@ document.querySelector('#email').addEventListener("blur", function(){
   ${address} ${address2}
   ${city} ${state} ${zip}
   ${country}`;
-  outputTag.innerHTML = shippingInfo;
+  outputShipping.innerHTML = shippingInfo;
 })
 document.querySelector('#address').addEventListener("blur", function(){
   address = document.getElementById('address').value;
@@ -85,7 +166,7 @@ document.querySelector('#address').addEventListener("blur", function(){
   ${address} ${address2}
   ${city} ${state} ${zip}
   ${country}`;
-  outputTag.innerHTML = shippingInfo;
+  outputShipping.innerHTML = shippingInfo;
 })
 document.querySelector('#address2').addEventListener("blur", function(){
   address2 = document.getElementById('address2').value;
@@ -95,7 +176,7 @@ document.querySelector('#address2').addEventListener("blur", function(){
   ${address} ${address2}
   ${city} ${state} ${zip}
   ${country}`;
-  outputTag.innerHTML = shippingInfo;
+  outputShipping.innerHTML = shippingInfo;
 })
 document.querySelector('#city').addEventListener("blur", function(){
   city = document.getElementById('city').value;
@@ -105,7 +186,7 @@ document.querySelector('#city').addEventListener("blur", function(){
   ${address} ${address2}
   ${city} ${state} ${zip}
   ${country}`;
-  outputTag.innerHTML = shippingInfo;
+  outputShipping.innerHTML = shippingInfo;
 })
 document.querySelector('#state').addEventListener("blur", function(){
   state = document.getElementById('state').value;
@@ -115,7 +196,7 @@ document.querySelector('#state').addEventListener("blur", function(){
   ${address} ${address2}
   ${city} ${state} ${zip}
   ${country}`;
-  outputTag.innerHTML = shippingInfo;
+  outputShipping.innerHTML = shippingInfo;
 })
 document.querySelector('#zip').addEventListener("blur", function(){
   zip = document.getElementById('zip').value;
@@ -125,7 +206,7 @@ document.querySelector('#zip').addEventListener("blur", function(){
   ${address} ${address2}
   ${city} ${state} ${zip}
   ${country}`;
-  outputTag.innerHTML = shippingInfo;
+  outputShipping.innerHTML = shippingInfo;
 })
 
 // Set up submit listener
@@ -154,6 +235,8 @@ submit.addEventListener("click", (event) => {
       `<section>
       <h1>Order Completed</h1>
       <h2>Order Summary</h2>
+      <p>${size} ${color} Paint Can
+      </p>
       <h3>Shipping Address</h3>
       <p>${name}
       ${email}
